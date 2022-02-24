a6bf1a1c235e09e03d217094d900d777651b2b73
Ema Zhedo Hu <55697560+Cioloic@users.noreply.github.com>
2022-02-24 20:17:39 +0100

    Add files via upload

--- parseur.py
+++ parseur.py
@@ -1,13 +1,52 @@
+class Contributeur :
+    
+    def __init__(self, name) :
+        self.name = name
+        self.skills = []
+    
+    def addSkill(self, skill) :
+        self.skills.append(skill)
+    
+
+class Skill :
+    
+    def __init__(self, name, lvl) :
+        self.name = name
+        self.lvl = lvl
+    
+    def lvlup(self) :
+        self.lvl = self.lvl + 1
+    
+
+
+
+
+
+
+
+
+
 def parseur(filename) :
     file = open(filename, "r")
     line = file.readline()
     line = line[:-1]
     entete = line.split(" ")
-    rues = []
-    for i in range(int(entete[2])) :
+    contribs = []
+    for i in range(int(entete[0])) :
         line = file.readline()
         line = line[:-1]
-        rues.append(line.split(" "))
+        contib_info = line.split(" ")
+        contrib = Contributeur(contrib_info[0])
+        for j in range(int(contrib_info[1])) :
+            line = file.readline()
+            line = line[:-1]
+            skill_info = line.split(" ")
+            skill = Skill(skill_info[0], skill_info[1])
+            contrib.addSkill(skill)
+            
+            # caca
+            
+            
     cars = []
     for i in range(int(entete[3])) :
         line = file.readline()
