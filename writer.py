e23f186ca5e388fa48beb616cc969dd7649798b4
Daniel K <danielkhamzen@gmail.com>
2022-02-24 21:28:58 +0100

    edit writer

--- writer.py
+++ writer.py
@@ -1,4 +1,4 @@
 def writeData(nameInputFile, listProject):
     with open('{nameInputFile}.txt', 'w') as f:
         for i in listProject:
-            f.write('{listProject[i]}')
+            f.write('{listProject[i].name} \n'.join(listProject[i].contrib))
