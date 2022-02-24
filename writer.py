72e5720ec9807c11c56a1ade575bc0f797304e10
Daniel K <danielkhamzen@gmail.com>
2022-02-24 21:20:21 +0100

    edit writer

--- writer.py
+++ writer.py
@@ -1,3 +1,4 @@
-def writeData(nameInputFile):
+def writeData(nameInputFile, listProject):
     with open('{nameInputFile}.txt', 'w') as f:
-        
+        for i in listProject:
+            f.write('{listProject[i]}')
