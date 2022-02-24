4641d5f689e9e939cbb14c7d374ede1d081aa7d6
Ema Zhedo Hu <55697560+Cioloic@users.noreply.github.com>
2022-02-24 19:50:49 +0100

    Add files via upload

---
+++ parseur.py
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+def parseur(filename) :
+    file = open(filename, "r")
+    line = file.readline()
+    line = line[:-1]
+    entete = line.split(" ")
+    rues = []
+    for i in range(int(entete[2])) :
+        line = file.readline()
+        line = line[:-1]
+        rues.append(line.split(" "))
+    cars = []
+    for i in range(int(entete[3])) :
+        line = file.readline()
+        line = line[:-1]
+        cars.append(line.split(" "))
+    donnees = Data(entete, rues, cars)
+    return(donnees)
